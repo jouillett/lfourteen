96e574d0ab7ff99fe14518c62d720658042b2540
jouillett <jouillett@example.com>
2026-08-03 14:44:43 +0900

    Update footer with bank account info

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,7 @@ export default function Footer() {
         <div className="flex flex-col gap-xs font-caption text-caption text-on-surface-variant text-left">
           <span>상호 기쁜하루 <span className="mx-1 opacity-50">|</span> 대표 조민균</span>
           <span>문의전화 031-755-1568 <span className="mx-1 opacity-50">|</span> 주소 경기도 성남시 분당구 궁내로22번길 16</span>
+          <span>은행 79423195492 카카오뱅크 (예금주: 조민균)</span>
           <span>사업자등록번호 691-23-02249</span>
           <span>통신판매업신고 신청중</span>
         </div>
