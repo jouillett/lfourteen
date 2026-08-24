04730e0e92b724d11f8dbb4eaf4aa1270271bcc5
jouillett <jouillett@example.com>
2026-08-24 10:47:02 +0900

    Enable site copy ban globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
       </head>
       <body className="bg-surface text-on-surface font-body-md">
+        <DisableCopy />
         {children}
       </body>
     </html>
